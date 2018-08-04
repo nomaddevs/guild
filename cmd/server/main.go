59aab2a4a6a16f5230ad3feb11f37e0e4f390ea7
tmunson <tmunson@wsu.edu>
2018-08-04 11:33:30 -0700

    update server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -45,6 +45,15 @@ func register(mux *http.ServeMux) {
 	for i := 0; i < len(staticRoutes); i++ {
 		loadStatic(mux, staticRoutes[i])
 	}
+
+	// Any other request, we should render our SPA's only html file,
+	// Allowing angular to do the routing on anything else other then the api
+	// and the files it needs for itself to work.
+	// Order here is critical. This html should contain the base tag like
+	// <base href="/"> *href here should match the HandleFunc path below
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../../web/html/index.html")
+	})
 }
 
 func init() {
@@ -73,7 +82,7 @@ func main() {
 	// Load API
 	mux := guild.Load()
 
-	// Register all static routes
+	// Register all static routes, including index
 	register(mux)
 
 	// Any other request, we should render our SPA's only html file,
@@ -81,9 +90,9 @@ func main() {
 	// and the files it needs for itself to work.
 	// Order here is critical. This html should contain the base tag like
 	// <base href="/"> *href here should match the HandleFunc path below
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../../web/html/index.html")
-	})
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "../../web/html/index.html")
+	// })
 
 	if !runTLS {
 		fmt.Println("TLS configuration not set. Falling back to HTTP...")
