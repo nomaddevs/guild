f6f502bcf1d00ee11bbc336b7413b42c0a323bec
tmunson <tmunson@wsu.edu>
2018-08-03 18:19:02 -0700

    Update installer

--- config/installer.go
+++ config/installer.go
@@ -3,9 +3,275 @@ package config
 import (
 	"errors"
 	"fmt"
+
+	"github.com/munsy/guild/config"
 )
 
 func Install() bool {
+
+	if "" != Key {
+		fmt.Printf("Key is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set Key.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for Key: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set Key.\n")
+				return false
+			}
+			Key = answer
+		}
+	}
+	if "" != Secret {
+		fmt.Printf("Secret is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set Secret.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for Secret: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set Secret.\n")
+				return false
+			}
+			Secret = answer
+		}
+	}
+	if "" != RedirectURL {
+		fmt.Printf("RedirectURL is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set RedirectURL.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for RedirectURL: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set RedirectURL.\n")
+				return false
+			}
+			RedirectURL = answer
+		}
+	}
+	if "" != Addr {
+		fmt.Printf("Addr is not set. Would you like to set it now (y/n, default n)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set Addr.\n")
+			return false
+		}
+
+		if strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for Addr: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set Addr.\n")
+				return false
+			}
+			Addr = answer
+		}
+	}
+	if "" != CertFile {
+		fmt.Printf("CertFile is not set. Would you like to set it now (y/n, default n)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set CertFile.\n")
+			return false
+		}
+
+		if strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for CertFile: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set CertFile.\n")
+				return false
+			}
+			CertFile = answer
+		}
+	}
+	if "" != KeyFile {
+		fmt.Printf("KeyFile is not set. Would you like to set it now (y/n, default n)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set KeyFile.\n")
+			return false
+		}
+
+		if strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for KeyFile: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set KeyFile.\n")
+				return false
+			}
+			KeyFile = answer
+		}
+	}
+	if "" != DBUsername {
+		fmt.Printf("DBUsername is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBUsername.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBUsername: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBUsername.\n")
+				return false
+			}
+			DBUsername = answer
+		}
+	}
+	if "" != DBPassword {
+		fmt.Printf("DBPassword is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBPassword.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBPassword: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBPassword.\n")
+				return false
+			}
+			DBPassword = answer
+		}
+	}
+	if "" != DBUnixsocketpath {
+		fmt.Printf("DBUnixsocketpath is not set. Would you like to set it now (y/n, default n)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBUnixsocketpath.\n")
+			return false
+		}
+
+		if strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBUnixsocketpath: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBUnixsocketpath.\n")
+				return false
+			}
+			DBUnixsocketpath = answer
+		}
+	}
+	if "" != DBHost {
+		fmt.Printf("DBHost is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBHost.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBHost: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBHost.\n")
+				return false
+			}
+			DBHost = answer
+		}
+	}
+	if "" != DBPort {
+		fmt.Printf("DBPort is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBPort.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBPort (default 3306): ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBPort.\n")
+				return false
+			}
+			if answer == "" {
+				DBPort = "3306"
+			}
+		}
+	}
+	if "" != DBName {
+		fmt.Printf("DBName is not set. Would you like to set it now (y/n, default y)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBName.\n")
+			return false
+		}
+
+		if answer == "" || strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBName: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBName.\n")
+				return false
+			}
+			DBName = answer
+		}
+	}
+	if "" != DBCharset {
+		fmt.Printf("DBCharset is not set. Would you like to set it now (y/n, default n)? ")
+		var answer string
+		_, err := fmt.Scanln(&answer)
+		if nil != err {
+			fmt.Printf("Error trying to set DBCharset.\n")
+			return false
+		}
+
+		if strings.Contains(answer, "y") {
+			answer = ""
+			fmt.Printf("New value for DBCharset: ")
+			_, err = fmt.Scanln(&answer)
+			if nil != err {
+				fmt.Printf("Error trying to set DBCharset.\n")
+				return false
+			}
+			DBCharset = answer
+		}
+	}
+
 	fmt.Printf("Creating %s...\n")
 
 	err := Write()
@@ -15,7 +281,7 @@ func Install() bool {
 		return false
 	}
 
-	fmt.Printf("Write succeeded.\n")
+	fmt.Printf("Succeeded writing file '%s'\n", TOMLFile)
 
 	fmt.Printf("Creating database '%s'\n", DBName)
 
--- database/mariadb.go
+++ database/mariadb.go
@@ -177,6 +177,122 @@ func (db *MariaDB) ReadNewsPosts() ([]int, []string, []string, []time.Time, []st
 	return ids, titles, bodys, dates, authors, nil
 }
 
+func (db *MariaDB) Install() error {
+	fmt.Printf("Creating table '%s.Applications...\n", db.Database)
+	err := db.createTableApplications()
+
+	if nil != err {
+		return err
+	}
+	fmt.Printf("Successfully created table '%s.Applications\n", db.Database)
+
+	fmt.Printf("Creating table '%s.NewsPosts...\n", db.Database)
+	err = db.createTableNewsPosts()
+
+	if nil != err {
+		return err
+	}
+	fmt.Printf("Successfully created table '%s.NewsPosts\n", db.Database)
+
+	return nil
+}
+
+func (db *MariaDB) createTableApplications() {
+	conn, err := sql.Open(db.DriverName(), db.ConnectionString())
+
+	if nil != err {
+		return err
+	}
+	defer conn.Close()
+
+	statement := `CREATE TABLE applications(
+		id BIGINT NOT NULL AUTO_INCREMENT, 
+		wowcharacter varchar(50) NOT NULL, 
+		email varchar(50) NOT NULL, 
+		realname varchar(50) NOT NULL, 
+		location varchar(100) NOT NULL, 
+		age TINYINT NOT NULL, 
+		gender varchar(20) NOT NULL, 
+		computerspecs varchar(500) NOT NULL, 
+		previousguilds varchar(500) NOT NULL, 
+		reasonsleavingguilds varchar(500) NOT NULL, 
+		whyjointhisguild varchar(500) NOT NULL, 
+		wowreferences varchar(500) NOT NULL, 
+		finalremarks varchar(500) NOT NULL, 
+		PRIMARY KEY (id)
+		) ENGINE = InnoDB;`
+
+	in, err := conn.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer in.Close() // Close the statement when we leave main() / the program terminates
+
+	in.Exec()
+
+	return nil
+}
+
+func (db *MariaDB) createTableNewsPosts() {
+	conn, err := sql.Open(db.DriverName(), db.ConnectionString())
+
+	if nil != err {
+		return err
+	}
+	defer conn.Close()
+
+	statement := `CREATE TABLE newsposts(
+		id BIGINT NOT NULL AUTO_INCREMENT, 
+		title VARCHAR(128) NOT NULL, 
+		body MEDIUMBLOB NOT NULL, 
+		date DATETIME NOT NULL, 
+		author VARCHAR(64) NOT NULL, 
+		PRIMARY KEY (id)
+		) ENGINE = InnoDB;`
+
+	in, err := conn.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer in.Close() // Close the statement when we leave main() / the program terminates
+
+	in.Exec()
+
+	return nil
+}
+
+func (db *MariaDB) createTableNewsPostComments() {
+	conn, err := sql.Open(db.DriverName(), db.ConnectionString())
+
+	if nil != err {
+		return err
+	}
+	defer conn.Close()
+
+	statement := `CREATE TABLE newscomments(
+		id BIGINT NOT NULL AUTO_INCREMENT, 
+		pid BIGINT NOT NULL, 
+		title VARCHAR(128) NOT NULL, 
+		body MEDIUMBLOB NOT NULL, 
+		date DATETIME NOT NULL, 
+		author VARCHAR(64) NOT NULL, 
+		PRIMARY KEY (id), 
+		CONSTRAINT ` + "`fk_parent_post`" + ` 
+		FOREIGN KEY (pid) REFERENCES newsposts (id) 
+		ON DELETE CASCADE ON UPDATE RESTRICT
+		) ENGINE = InnoDB;`
+
+	in, err := conn.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer in.Close() // Close the statement when we leave main() / the program terminates
+
+	in.Exec()
+
+	return nil
+}
+
 /*
 SQL Queries
 
