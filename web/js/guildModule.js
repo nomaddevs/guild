07b96a8d92db4f2b08aad345b1b0f814eaf88849
Tim Munson <tmunson@wsu.edu>
2018-08-05 06:10:46 +0000

    fix app page

--- web/js/guildModule.js
+++ web/js/guildModule.js
@@ -27,6 +27,8 @@ app.controller("guildController", ['$scope', '$http', '$cookies', '$location', f
 	$scope.User = {
 		BattleTag: "",
 		LoggedIn: false,
+		Applied: false,
+		Characters: [],
 	};
 
 	$scope.Guild = [];
@@ -50,11 +52,21 @@ app.controller("guildController", ['$scope', '$http', '$cookies', '$location', f
 			var token = $cookies.get("token");
 			if(!angular.isUndefined(token)) {
 				$http.get("/api/beta/user")
-				.then(function (response) {
+				.then(function(response) {
     				$scope.User.BattleTag = response.data.battletag;
     				$scope.User.LoggedIn = true;
 				}, function (response) {
 					$scope.User.LoggedIn = false;
+				});
+
+				$http.get("/api/beta/apply")
+				.then(function(response) {
+				$scope.User.Characters = response.data.characters.filter(function(x) {
+					return x.level >= 110;
+				});
+				console.log(response.data);
+				}, function(response) {
+					console.log(response);
 				});
 			}
 		}
