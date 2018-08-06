da9672548def5741f696e163176e3b55afac1adc
tmunson <tmunson@wsu.edu>
2018-08-05 20:50:28 -0700

    start app cli

---
+++ cmd/applicants/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Select action: ")
+	fmt.Println("0) Quit")
+	fmt.Println("1) View all applicants")
+	fmt.Println("2) View applicant by ID")
+	fmt.Println("3) Accept/Reject an applicant")
+	fmt.Println("4) Purge an applicant")
+	fmt.Printf("> ")
+
+	var s string
+	fmt.Scanln(&s)
+
+	i, err := strconv.Atoi(s)
+
+	if nil != err {
+		fmt.Println("Invalid entry")
+		return
+	}
+
+	switch i {
+	case 0:
+		return
+	case 1:
+		fmt.Println("case 1")
+		break
+	case 2:
+		fmt.Println("case 2")
+		break
+	case 3:
+		fmt.Println("case 3")
+		break
+	case 4:
+		fmt.Println("case 4")
+		break
+	default:
+		fmt.Println("invalid")
+		break
+	}
+}
