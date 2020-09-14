92165119a65aee26ce49e1e9a8bee32c750c2734
Munsy <tmunson@wsu.edu>
2020-09-14 16:29:38 -0700

    make things pretty

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { Recruitment } from './recruitment';
 
+declare var $ : any;
+//import * as $ from 'jquery';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,16 +15,37 @@ export class AppComponent {
 
   constructor() {
   	this.lfm = new Recruitment();
-  	this.lfm.SetStatus('DeathKnight', '*');
-  	this.lfm.SetStatus('DemonHunter', '*');
-  	this.lfm.SetStatus('Druid', '*');
-  	this.lfm.SetStatus('Hunter', '*');
+    this.lfm.SetStatus('DeathKnight', 'Frost');
+    this.lfm.SetStatus('DeathKnight', 'Unholy');
+  	this.lfm.SetStatus('DemonHunter', 'Havoc');
+  	this.lfm.SetStatus('Druid', 'Restoration');
+  	this.lfm.SetStatus('Hunter', 'Beast Mastery');
   	this.lfm.SetStatus('Mage', '*');
   	this.lfm.SetStatus('Monk', 'Mistweaver');
-  	this.lfm.SetStatus('Paladin', '*');
+    this.lfm.SetStatus('Paladin', 'Retribution');
+    this.lfm.SetStatus('Paladin', 'Holy');
   	this.lfm.SetStatus('Priest', '*');
+    this.lfm.SetStatus('Rogue', 'Subtlety');
   	this.lfm.SetStatus('Shaman', '*');
   	this.lfm.SetStatus('Warlock', '*');
   	this.lfm.SetStatus('Warrior', '*');
+  }
+
+  public about() {
+    $('#collapse-about').collapse('show');
+    $('#collapse-recruitment').collapse('hide');
+    $('#collapse-faq').collapse('hide');
+  }
+
+  public recruitment() {
+    $('#collapse-about').collapse('hide');
+    $('#collapse-recruitment').collapse('show');
+    $('#collapse-faq').collapse('hide');
+  }
+
+  public faq() {
+    $('#collapse-about').collapse('hide');
+    $('#collapse-recruitment').collapse('hide');
+    $('#collapse-faq').collapse('show');
   }
 }
